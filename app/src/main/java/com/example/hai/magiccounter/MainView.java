96ecf7ab45b1553ca441d6e7b0507646a172285c
haiquangtran <hai.quang.tran@gmail.com>
2015-03-15 23:25:26 +1300

    Added comments in the code, disabled orientation changing to landscape, and added a new game button

--- app/src/main/java/com/example/hai/magiccounter/MainView.java
+++ app/src/main/java/com/example/hai/magiccounter/MainView.java
@@ -1,12 +1,15 @@
 package com.example.hai.magiccounter;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.NoSuchElementException;
 
 
 public class MainView extends Activity {
@@ -18,8 +21,8 @@ public class MainView extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
 
-        player1 = new Player(20, "Player 1");
-        player2 = new Player(20, "Player 2");
+        // Create a new game
+        newGame(this.findViewById(android.R.id.content));
     }
 
 
@@ -45,37 +48,99 @@ public class MainView extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     *  Increments the life of the given player when
+     *  the add life button has been pressed and
+     *  displays the updated life score on the screen.
+     *
+     * @param view
+     */
     public void incrementLife(View view) {
-        switch(view.getId()){
+        int elementId = view.getId();
+
+        switch (elementId) {
+            // Player 1
             case R.id.buttonAdd1:
                 player1.incrementScore();
-                TextView player1View = (TextView) findViewById(R.id.player1Life);
-                player1View.setText(player1.getScore());
+                updateLifeTextView(R.id.player1Life);
                 break;
+            // Player 2
             case R.id.buttonAdd2:
                 player2.incrementScore();
-                TextView player2View = (TextView) findViewById(R.id.player2Life);
-                player2View.setText(player2.getScore());
+                updateLifeTextView(R.id.player2Life);
                 break;
             default:
-                throw new NullPointerException();
+                throw new NoSuchElementException();
         }
     }
 
+    /**
+     *  Decrements the life of the given player when
+     *  the minus life button has been pressed and
+     *  displays the updated life score on the screen.
+     *
+     * @param view
+     */
     public void decrementLife(View view) {
-        switch(view.getId()){
+        int elementId = view.getId();
+
+        switch (elementId) {
+            // Player 1
             case R.id.buttonMinusOne:
                 player1.decrementScore();
-                TextView player1View = (TextView) findViewById(R.id.player1Life);
-                player1View.setText(player1.getScore());
+                updateLifeTextView(R.id.player1Life);
                 break;
+            // Player 2
             case R.id.buttonMinus2:
                 player2.decrementScore();
-                TextView player2View = (TextView) findViewById(R.id.player2Life);
+                updateLifeTextView(R.id.player2Life);
+                break;
+            default:
+                throw new NoSuchElementException();
+        }
+
+    }
+
+    /**
+     * Updates the life score text view with the the players score given the player id
+     *
+     * @param playerId
+     */
+    public void updateLifeTextView(int playerId) {
+        switch (playerId) {
+            case R.id.player1Life:
+                TextView player1View = (TextView) findViewById(playerId);
+                player1View.setText(player1.getScore());
+                break;
+            case R.id.player2Life:
+                TextView player2View = (TextView) findViewById(playerId);
                 player2View.setText(player2.getScore());
                 break;
             default:
-                throw new NullPointerException();
+                throw new NoSuchElementException();
         }
     }
+
+    /**
+     *  Creates a new game with 2 players starting with 20 life
+     */
+    public void newGame(View view) {
+        player1 = new Player(20, "Player 1");
+        player2 = new Player(20, "Player 2");
+        // Update the views
+        updateLifeTextView(R.id.player1Life);
+        updateLifeTextView(R.id.player2Life);
+    }
+
+    /**
+     * Disables the phones ability to switch orientation to landscape.
+     *
+     * @param newConfig
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+
 }
